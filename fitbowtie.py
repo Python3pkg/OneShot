665b0ec96ce7e780b76f5d2ca236461c7180ede3
Joel Frederico <joelfred@slac.stanford.edu>
2013-11-13 16:01:39 -0800

    Unpushed updates.

--- fitbowtie.py
+++ fitbowtie.py
@@ -1,46 +1,83 @@
 #!/usr/bin/env python
 import numpy as np
+import copy
+import collections as col
+import mytools as mt
 
 # Fit bowtie {{{
-def fitbowtie(beamline,davg,stddevsq,filt,T,twiss,emitx):
-	betax=twiss[0]
-	alphax=twiss[1]
-	gammax=twiss[2]
-	y=stddevsq[filt]
-	y=y[np.newaxis]
-	gamma = (1+davg[filt])*40000
-	X            = np.zeros([len(gamma),3])
-	# spot         = np.zeros(len(gamma))
-	spotexpected = np.zeros(len(gamma))
-	R = np.zeros([6,6,len(gamma)])
-	betaf = np.zeros(len(gamma))
+def fitbowtie(beamline,x,y,filt,T,twiss,emitx,error=None,verbose=False):
+	beamline_manip = copy.deepcopy(beamline)
+	betax          = twiss[0]
+	alphax         = twiss[1]
+	gammax         = twiss[2]
+	y              = y[filt]
+	y              = y[np.newaxis]
+	gamma          = (1+x[filt])*40000
+	X              = np.zeros([len(gamma),3])
+	spotexpected   = np.zeros(len(gamma))
+	R              = np.zeros([6,6,len(gamma)])
+	betaf          = np.zeros(len(gamma))
 	
 	for i,g in enumerate(gamma):
-		beamline.change_energy(g)
-		beamline.calc_mat()
-		R11 = beamline.R[0,0]
-		R12 = beamline.R[0,1]
-		R[:,:,i] = beamline.R
-		X[i,0] = R11*R11
-		X[i,1] = 2*R11*R12
-		X[i,2] = R12*R12
-		T2 = np.dot(np.dot(R[0:2,0:2,i],T),np.transpose(R[0:2,0:2,i]))
-		betaf[i] = T2[0,0]
+		beamline_manip.change_energy(g)
+		beamline_manip.calc_mat()
+		R11             = beamline_manip.R[0,0]
+		R12             = beamline_manip.R[0,1]
+		R[:,:,i]        = beamline_manip.R
+		X[i,0]          = R11*R11
+		X[i,1]          = 2*R11*R12
+		X[i,2]          = R12*R12
+		T2              = np.dot(np.dot(R[0:2,0:2,i],T),np.transpose(R[0:2,0:2,i]))
+		betaf[i]        = T2[0,0]
 		spotexpected[i] = np.sqrt((R11*R11*betax - 2*R11*R12*alphax + R12*R12*gammax)*emitx)
 	
-	beta = np.dot(np.linalg.pinv(X) , y.transpose())
-	# print beta
+	# beta is the best solution of beam parameters:
+	# sig^2 = R11^2 <x^2> + 2 R11 R12 <xx'> + R12^2 <x'^2>
+	# beta[0,0] = <x^2>
+	# beta[1,0] = <xx'>
+	# beta[2,0] = <x'^2>
+
+	X_unweighted = copy.deepcopy(X)
+	y_unweighted = copy.deepcopy(y)
+	if ( error != None ):
+	# if ( False ):
+		for i,el in enumerate(X):
+			X[i,:] = el/error[i]
+		y_err = y/error
+
+	# This is the linear least squares matrix formalism
+	y_err = y_err.transpose()
+	beta  = np.dot(np.linalg.pinv(X) , y_err)
+	covar = np.linalg.inv(np.dot(np.transpose(X),X))
 	
 	emit = np.sqrt( beta[0,0] * beta[2,0] - np.square(beta[1,0]) )
-	beta0 = beta[0,0]/emit
-	gamma0 = beta[2,0]/emit
-	alpha0 = -np.sign(beta[1,0])*np.sqrt(beta0*gamma0-1)
-	print 'Emittance fit:\t{}.'.format(emit)
-	print 'Normalized emittance fit:\t{}.'.format(emit*40000)
-	print 'Initial beta fit:\t{}.'.format(beta0)
-	print 'Initial alpha fit:\t{}.'.format(alpha0)
-	print 'Initial gamma fit:\t{}.'.format(gamma0)
-	print 'Initial spot from fit:\t{}.'.format(np.sqrt(beta[0,0]))
-	out = [spotexpected,X,beta]
+	del_emit_sq = np.power(1/(2*emit),2) * \
+		(
+			np.power(beta[2,0] * covar[0,0]   ,2) +
+			np.power(2*beta[1,0] * covar[1,1] ,2) +
+			np.power(beta[0,0] * covar[2,2]   ,2) # +
+			# 2 * (-beta[2,0] * 2 * beta[1,0]) * covar[0,1] +
+			# 2 * (beta[2,0] * beta[0,0]) * covar[0,2] +
+			# 2 * (-2*beta[1,0] * beta[0,0]) * covar[1,2]
+		)
+
+	chisq_red = mt.chisquare(y.transpose(),np.dot(X_unweighted,beta),error,ddof=3,verbose=verbose)
+# def chisquare(observe,expect,error,ddof,verbose=True):
+
+	if verbose:
+		print 'Emittance error is:\t\t{}.'.format(np.sqrt(del_emit_sq))
+		beta0 = beta[0,0]/emit
+		gamma0 = beta[2,0]/emit
+		alpha0 = -np.sign(beta[1,0])*np.sqrt(beta0*gamma0-1)
+		print 'Emittance fit:\t\t\t{}.'.format(emit)
+		print 'Normalized emittance fit:\t{}.'.format(emit*40000)
+		print 'Initial beta fit:\t\t{}.'.format(beta0)
+		print 'Initial alpha fit:\t\t{}.'.format(alpha0)
+		print 'Initial gamma fit:\t\t{}.'.format(gamma0)
+		print 'Initial spot from fit:\t\t{}.'.format(np.sqrt(beta[0,0]))
+
+	output = col.namedtuple('fitbowtie_output',['spotexpected','X','X_unweighted','beta','covar','chisq_red'])
+	out = output(spotexpected,X,X_unweighted,beta,covar,chisq_red)
+
 	return out
 #}}}
--- plotfit.py
+++ plotfit.py
@@ -3,8 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import mytools as mt
 
-def plotfit(filt,davg,stddev,beta,X,spotexpected,top,elepath):
-	plt.plot(davg[filt]+1,stddev[filt]*1e6,'.',davg[filt]+1,np.sqrt(np.dot(X,beta))*1e6,'-',davg[filt]+1,spotexpected*1e6,'-')
-	mt.addlabel(top,'$E/E_0$','$\sigma_x$ [$\mu$m]')
+def plotfit(filt,x,y,beta,X,spotexpected,top,elepath='figs',error=None):
+	# Recast as fractional energy
+	e_frac=x[filt]+1
+	# Plot data with error bars
+	plt.errorbar(e_frac,y[filt]*(1e3**2),error*(1e3**2),fmt='.-')
+
+	# Plot fits
+	# plt.plot(e_frac,np.sqrt(np.dot(X,beta))*1e6,'-')
+	plt.plot(e_frac,np.dot(X,beta)*(1e3**2),'-')
+
+	mt.addlabel(top,'$E/E_0$','$\sigma_x^2$ [mm$^2$]')
 	# plt.legend(['Simulated Data','Fit to Data','Expected (via\nInitial Conditions)'])
+	plt.legend(['Simulated Data','Fit to Data'])
 	mt.graphics.savefig(top,elepath)
