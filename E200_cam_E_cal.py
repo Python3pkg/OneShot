f2107cceebdfa3aea69b2e37bc5527fbce76b087
Joel Frederico <joelfred@slac.stanford.edu>
2014-02-19 17:34:07 -0800

    Updated with energy axis routine

---
+++ E200_cam_E_cal.py
@@ -0,0 +1,38 @@
+def E200_cam_E_cal(h5file,y,res)
+	# ====================================
+	# Calibration depends on good orbit through quads,
+	# spectrometer magnet strength, and ...?
+	# ====================================
+
+	# ====================================
+	# Default energy and corresponding y location
+	# ====================================
+	E0 = 20.35;
+	# y0 determined from ROI'd centroid of E200_11469
+	# step 6, image 32.
+	# y0 = 476.5593;
+	y0 = 456.149271788;
+	res=res*1e-6;
+	
+	# ====================================
+	# Get the magnet strength
+	# ====================================
+	B_spect = h5file[h5file['data']['raw']['scalars']['LI20_LGPS_3330_BDES']['dat'][0,0]][0,0]
+
+	# ====================================
+	# The dispersion at y0
+	# ====================================
+	etay = 62e-3 * (B_spect/20.35) * (9.28/10.33);
+	yc = y0 + etay/res
+
+	# ====================================
+	# Convert to energy axis
+	# E_axis = -etay*E0./(y-y0);
+	# E_axis = E0./( 1-((y-y0)/etay) );
+	# E_axis = E0*( 1- (y-y0)/etay );
+	E_axis = -etay*E0./( (y-yc)*res );
+
+	return E_axis
+
+	display(-etay*(0.01)+y0);
+	display(-etay*(-0.01)+y0);
--- __init__.py
+++ __init__.py
@@ -6,3 +6,4 @@ from fitbowtie import fitbowtie
 from tradquadscan import tradquadscan
 import beamlines
 from fitBeamlineScan import fitBeamlineScan
+from E200_cam_E_cal import E200_cam_E_cal
